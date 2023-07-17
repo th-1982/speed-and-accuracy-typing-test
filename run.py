2cc4d848c1cb4c0e8a4f38a7163a310b5bd63cb1
Theresa Wolff <theresawolff4@gmail.com>
2023-07-17 22:55:44 +0000

    Wire up APIs and confirm access to google sheet

--- run.py
+++ run.py
@@ -1,1 +1,28 @@
+"""This program runs a speed and accuracy typing test."""
+
+import time
+from os import system, name
+from difflib import SequenceMatcher
+from statistics import mean
+from ast import literal_eval
+
+import gspread
+from google.oauth2.service_account import Credentials
+
+from wonderwords import RandomSentance
+from colorama import Fore, Style
+import pandas as pd
+
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('Speed-and-accuracy-test')
+
 
